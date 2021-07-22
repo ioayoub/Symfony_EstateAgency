185560ea602a7446b910b0911a59f104d53cc5b4
ioayoub <idrissio.ayoub@gmail.com>
2021-07-22 18:07:47 +0200

    PAGINATION with knpBundle

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use App\Entity\Property;
 use App\Repository\PropertyRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PropertyController extends AbstractController
 {
@@ -21,12 +23,23 @@ class PropertyController extends AbstractController
     /**
      * @Route("/biens", name="property_index")
      */
-    public function index(): Response
+    public function index(PaginatorInterface $paginator, Request $request): Response
     {
 
+        //Créer entité qui va representer recherche nbPices min et prix max
+        //creer un formulaire
+        //Gerer le traitement dans le controlleur
+        //passe en parametre findAlVisibleQuery
+
+        $properties = $paginator->paginate(
+            $this->repo->findAllVisibileQuery(),
+            $request->query->getInt('page', 1),
+            12
+        );
+
         return $this->render('property/index.html.twig', [
-            'controller_name' => 'PropertyController',
             'current_menu' => 'properties',
+            'properties' => $properties
         ]);
     }
 
